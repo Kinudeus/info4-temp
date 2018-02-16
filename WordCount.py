3bb9668e65312c282bc2d6e20d03750e92e8a743
Shouma Kawakami <e155705@ie.u-ryukyu.ac.jp>
2018-02-16 10:44:56 +0900

    add word count

--- WordCount.py
+++ WordCount.py
@@ -1,10 +1,14 @@
 import numpy as np
 import pandas as pd
 import doctest
+from collections import Counter
 
 def wordCount(arg1):
     arg1 = input('任意の英文を入力してください')
     print(arg1)
+
+    counter = Counter(arg1)
+    print(dict(counter))
 
 wordCount(1)
 
