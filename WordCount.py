e371cb42487154dcb8ac82e812ef1f1ceda57f6d
Shouma Kawakami <e155705@ie.u-ryukyu.ac.jp>
2018-02-16 10:55:23 +0900

    addMeCab

--- WordCount.py
+++ WordCount.py
@@ -2,13 +2,19 @@ import numpy as np
 import pandas as pd
 import doctest
 from collections import Counter
+import MeCab
+
 
 def wordCount(arg1):
     arg1 = input('任意の英文を入力してください')
     print(arg1)
 
-    counter = Counter(arg1)
-    print(dict(counter))
+    m = MeCab.Tagger()
+    print(m.parse(arg1))
+
+
+#    counter = Counter(arg1)
+#    print(dict(counter))
 
 wordCount(1)
 
