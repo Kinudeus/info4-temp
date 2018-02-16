59a3272ca1e0df77bb45a3d452cfdfe6369e663f
Shouma Kawakami <e155705@ie.u-ryukyu.ac.jp>
2018-02-16 11:30:58 +0900

    add Count kai

--- WordCount.py
+++ WordCount.py
@@ -1,5 +1,7 @@
-import numpy as np
-import pandas as pd
+#/Users/e155705/Work/info4/info4kai
+# coding: UTF-8
+#https://github.com/Shouma-K/info4-temp
+
 import doctest
 from collections import Counter
 import MeCab
@@ -13,13 +15,16 @@ def wordCount(arg1):
 #    print(m.parse(arg1))
 #    print(m.Counter(arg1))
 
-    words = re.split(r'\s|\,|\.|\(|\)', arg1.lower())
-#    words = m.parse(words)
+#    words = re.split(r'\s|\,|\.|\(|\|)', arg1.lower())
+    word = re.sub('[.],[,]','',arg1.lower())
+    words = re.split(r'\s|\,|\.|\(|\)', word.lower()) 
+#    words = re.split(r'\s|\,|\.|\(|\)', arg1.lower())
+ #   mecabWords = m.parse(words)
     counter = Counter(words)
     print(counter)
-
 #    counter = Counter(arg1)
 #    print(dict(counter))
+
 
 wordCount(1)
 
