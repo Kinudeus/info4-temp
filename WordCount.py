2b1d91238e79dd10255ea72bac980a82b957bd51
Shouma Kawakami <e155705@ie.u-ryukyu.ac.jp>
2018-02-16 11:04:12 +0900

    new count function

--- WordCount.py
+++ WordCount.py
@@ -3,15 +3,20 @@ import pandas as pd
 import doctest
 from collections import Counter
 import MeCab
-
+import re 
 
 def wordCount(arg1):
     arg1 = input('任意の英文を入力してください')
     print(arg1)
 
-    m = MeCab.Tagger()
-    print(m.parse(arg1))
+#    m = MeCab.Tagger()
+#    print(m.parse(arg1))
+#    print(m.Counter(arg1))
 
+    words = re.split(r'\s|\,|\.|\(|\)', arg1.lower())
+#    words = m.parse(words)
+    counter = Counter(words)
+    print(counter)
 
 #    counter = Counter(arg1)
 #    print(dict(counter))
